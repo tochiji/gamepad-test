53116ce884e0c6ca86f8c7fc32e3c7aa5224a4d3
tochiji <7910558+tochiji@users.noreply.github.com>
2024-04-14 23:25:02 +0900

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,91 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [leftStickX, setLeftStickX] = useState(0);
+	const [leftStickY, setLeftStickY] = useState(0);
+	const [frameCount, setFrameCount] = useState(0);
+	const canvasRef = useRef<HTMLCanvasElement>(null);
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		const ctx = canvas?.getContext("2d");
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		// ゲームパッドの状態を更新する関数
+		const updateGamepad = () => {
+			// フレームカウントを更新
+			setFrameCount((frameCount) => frameCount + 1);
+			const gamepads = navigator.getGamepads();
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+			for (let i = 0; i < gamepads.length; i++) {
+				const gamepad = gamepads[i];
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+				if (gamepad) {
+					// 左スティックの入力を取得
+					const leftStickX = gamepad.axes[0];
+					const leftStickY = gamepad.axes[1];
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+					setLeftStickX(leftStickX);
+					setLeftStickY(leftStickY);
+
+					if (!ctx || !canvas) {
+						return;
+					}
+
+					// canvasの背景色を黒に設定
+					ctx.fillStyle = "black";
+					ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+					// 円を描画
+					ctx.beginPath();
+					ctx.arc(canvas.width / 2, canvas.height / 2, 100, 0, 2 * Math.PI);
+					ctx.strokeStyle = "white"; // 変更
+					ctx.lineWidth = 2;
+					ctx.stroke();
+
+					// スティックの方向を赤いポイントとして表示
+					if (Math.abs(leftStickX) > 0.05 || Math.abs(leftStickY) > 0.05) {
+						const angle = Math.atan2(leftStickY, leftStickX);
+						const pointX = canvas.width / 2 + 100 * Math.cos(angle);
+						const pointY = canvas.height / 2 + 100 * Math.sin(angle);
+						ctx.beginPath();
+						ctx.arc(pointX, pointY, 20, 0, 2 * Math.PI);
+						ctx.fillStyle = "red";
+						ctx.fill();
+					}
+				}
+			}
+
+			// 次のフレームでゲームパッドの状態を更新
+			requestAnimationFrame(updateGamepad);
+		};
+
+		// イベントリスナーを追加
+		// window.addEventListener("gamepadconnected", handleGamepadConnected);
+		// window.addEventListener("gamepaddisconnected", handleGamepadDisconnected);
+
+		// ゲームパッドの状態の更新を開始
+		updateGamepad();
+
+		// クリーンアップ関数
+		return () => {
+			// イベントリスナーを削除
+			// window.removeEventListener("gamepadconnected", handleGamepadConnected);
+			// window.removeEventListener(
+			// "gamepaddisconnected",
+			// handleGamepadDisconnected,
+			// );
+		};
+	}, []);
+
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<div>
+				<canvas ref={canvasRef} width="400" height="400" />
+				<p>Left Stick X: {leftStickX}</p>
+				<p>Left Stick Y: {leftStickY}</p>
+				<p>frame: {frameCount}</p>
+			</div>
+		</main>
+	);
 }
